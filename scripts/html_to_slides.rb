ddfa1beb1403e4819c43933c01925f997780716b
Murray Steele <muz@h-lame.com>
2021-01-16 21:36:42 +0000

    Tidy up html_to_slides script
    We're about to introduce more complex handling for talks (the community
    one) that have _parts_ as well as slides, so it's useful to tidy this
    script up because we want to re-use parts of it to support that structure.

--- scripts/html_to_slides.rb
+++ scripts/html_to_slides.rb
@@ -1,5 +1,103 @@
 require 'nokogiri'
 require 'yaml'
+require 'fileutils'
+
+def write_slide(item, slide_number, path)
+  frontmatter = item[:frontmatter].transform_keys(&:to_s).to_yaml
+  file_name = "#{slide_number}.md"
+  file_name << '.erb' if item[:config][:erb_used]
+  File.open(File.join(path, file_name), 'w') do |f|
+    f.puts(frontmatter)
+    f.puts('---')
+    f.puts item[:body]
+    if item[:config][:abbrevs].any?
+      f.puts
+      f.puts item[:config][:abbrevs].map { |abbrev, definition| "*[#{abbrev}]: #{definition}" }.join("\n")
+    end
+  end
+end
+
+def extract_slide_content_from_doc(item, doc)
+  slide_content = doc.css("##{item[:id]}")
+  image = slide_content.css('figure.slides img')
+  item[:slide] = {
+    image_url: image.attribute('src').value,
+    image_alt: image.attribute('alt').value,
+    image_title: image.attribute('title').value
+  }
+  item[:body] = slide_content.css('.transcript').children.to_html
+  item
+end
+
+def convert_html_to_markdown_line_pass_1(line, config)
+  line
+    .gsub('<p>', '')
+    .gsub('</p>',"\n\n")
+    .gsub(/<a href="([^"]+)">(.+?)<\/a>/, '[\2](\1)')
+    .gsub(/<a id="fn-(\d+)-return" href="#fn-\1"><sup>\1<\/sup><\/a>/) do
+     reference = Regexp.last_match[1]
+     config[:erb_used] = true
+     "<% footnote_reference #{reference} %>"
+    end
+    .gsub(/<code>(.+?)<\/code>/, '`\1`')
+    .gsub(/<(em|i)>(.+?)<\/\1>/, '*\2*')
+    .gsub(/<(strong|b)>(.+?)<\/\1>/, '**\2**')
+    .gsub(/<abbr title="([^"]+)">(.+?)<\/abbr>s?/) do
+      abbrev = Regexp.last_match[2]
+      plural_abbrev = "#{abbrev}s"
+      definition = Regexp.last_match[1]
+      if config[:abbrevs].key? abbrev && config[:abbrevs][abbrev] != definition
+        puts "Uh oh, already defined #{abbrev} as #{abbrevs[abbrev]} not as #{definition}"
+      else
+        config[:abbrevs][plural_abbrev] = definition
+        config[:abbrevs][abbrev] = definition
+      end
+      Regexp.last_match[0].end_with?('s') ? plural_abbrev : abbrev
+    end
+end
+
+def convert_html_to_markdown_line_pass_2(line, _config)
+  line
+    .gsub(/<ol>(.+?)<\/ol>/) { Regexp.last_match[1].gsub(/<li>(.+?)<\/li>/, "1. \\1\n") + "\n" }
+    .gsub(/<ul>(.+?)<\/ul>/) { Regexp.last_match[1].gsub(/<li>(.+?)<\/li>/, "* \\1\n") + "\n" }
+end
+
+def convert_content_to_markdown(item)
+  body = item.delete(:body)
+  config = {
+    abbrevs: {},
+    erb_used: false
+  }
+  body = body.lines
+    .map(&:strip)
+    .map { |l| convert_html_to_markdown_line_pass_1(l, config) }
+    .join "\n"
+    .strip
+  body = body.lines
+    .map { |l| convert_html_to_markdown_line_pass_2(l, config) }
+    .join "\n"
+    .strip
+  {body: body, config: config, frontmatter: item}
+end
+
+def extract_slides(links, path, doc)
+  path = File.join(path, 'slides')
+  FileUtils.mkdir_p path
+  links
+    .select { |item| item.attribute('href').value.match?(/\A\#slide/) }
+    .map { |item| extract_slide(item, doc) }
+    .each.with_index { |item, idx| write_slide(item, idx+1, path) }
+end
+
+def extract_slide(link, doc)
+  convert_content_to_markdown(
+    extract_slide_content_from_doc(
+      {title: link.text.gsub(/\A\d+\:\s+/, ''), id: link.attribute('href').value.tr('#','') },
+      doc
+    )
+  )
+end
+
 
 puts ARGV[0]
 
@@ -8,68 +106,4 @@ talk_index = File.read(File.join(ARGV[0], 'index.html'))
 doc = Nokogiri::HTML(talk_index)
 
 toc = doc.css('#menu ul li [href]')
-toc.select { |item| item.attribute('href').value.match?(/\A\#slide/) }
-   .map { |item| {title: item.text.gsub(/\A\d+\:\s+/, ''), id: item.attribute('href').value.tr('#','') } }
-   .map do |item|
-     slide_content = doc.css("##{item[:id]}")
-     image = slide_content.css('figure.slides img')
-     item[:slide] = {
-       image_url: image.attribute('src').value,
-       image_alt: image.attribute('alt').value,
-       image_title: image.attribute('title').value
-     }
-     item[:body] = slide_content.css('.transcript').children.to_html
-     item
-   end
-   .each.with_index do |item, idx|
-     body = item.delete(:body)
-     abbrevs = {}
-     erb_used = false
-     body = body.lines
-       .map(&:strip)
-       .map do |l|
-         l.gsub('<p>', '')
-          .gsub('</p>',"\n\n")
-          .gsub(/<a href="([^"]+)">(.+?)<\/a>/, '[\2](\1)')
-          .gsub(/<a id="fn-(\d+)-return" href="#fn-\1"><sup>\1<\/sup><\/a>/) do
-            reference = Regexp.last_match[1]
-            erb_used = true
-            "<% footnote_reference #{reference} %>"
-          end
-          .gsub(/<code>(.+?)<\/code>/, '`\1`')
-          .gsub(/<(em|i)>(.+?)<\/\1>/, '*\2*')
-          .gsub(/<(strong|b)>(.+?)<\/\1>/, '**\2**')
-          .gsub(/<abbr title="([^"]+)">(.+?)<\/abbr>s?/) do
-            abbrev = Regexp.last_match[2]
-            plural_abbrev = "#{abbrev}s"
-            definition = Regexp.last_match[1]
-            if abbrevs.key? abbrev && abbrevs[abbrev] != definition
-              puts "Uh oh, already defined #{abbrev} as #{abbrevs[abbrev]} not as #{definition}"
-            else
-              abbrevs[plural_abbrev] = definition
-              abbrevs[abbrev] = definition
-            end
-            Regexp.last_match[0].end_with?('s') ? plural_abbrev : abbrev
-          end
-       end
-       .join "\n"
-       .strip
-     body = body.lines.map do |l|
-         l.gsub(/<ol>(.+?)<\/ol>/) { Regexp.last_match[1].gsub(/<li>(.+?)<\/li>/, "1. \\1\n") + "\n" }
-          .gsub(/<ul>(.+?)<\/ul>/) { Regexp.last_match[1].gsub(/<li>(.+?)<\/li>/, "* \\1\n") + "\n" }
-       end
-       .join "\n"
-       .strip
-     frontmatter = item.transform_keys(&:to_s).to_yaml
-     file_name = "#{idx+1}.md"
-     file_name << '.erb' if erb_used
-     File.open(File.join(ARGV[0], 'slides', file_name), 'w') do |f|
-       f.puts(frontmatter)
-       f.puts('---')
-       f.puts body
-       if abbrevs.any?
-         f.puts
-         f.puts abbrevs.map { |abbrev, definition| "*[#{abbrev}]: #{definition}"}.join("\n")
-       end
-     end
-   end
+extract_slides(toc, ARGV[0], doc)
