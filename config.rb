f09f8baceda53f4b41dbc4e1e8b1982ee9eec1de
Murray Steele <muz@h-lame.com>
2021-01-20 22:26:24 +0000

    Push markdown through erb first in the markdown helper
    Because we might want to use our footnote or figure helpers in markdown we
    write that will be rendered via this helper, rather than via a file.

--- config.rb
+++ config.rb
@@ -95,7 +95,12 @@ helpers do
   def markdown(inline: false, strip_tags: false, &block)
     raise ArgumentError, "Missing block" unless block_given?
     content = capture_html(&block)
-    rendered = Tilt['markdown'].new(context: @app) { content }.render
+    context = @app.template_context_class.new(@app, {}, {layout: false})
+
+    rendered = ['erb', 'md'].reduce(content) do |rendered, template|
+      content_renderer = ::Middleman::FileRenderer.new(@app, "inline_markdown_snippet.#{template}")
+      content_renderer.render({}, {template_body: rendered, layout: false}, context)
+    end
     rendered = rendered.gsub('<p>', '').gsub("</p>", '').gsub("\n", '') if inline
     rendered = strip_tags(rendered) if strip_tags
     concat rendered
