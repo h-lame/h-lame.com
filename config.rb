6528c17f57d346e5e2a3356e94c6c88f1974a5cd
Murray Steele <muz@h-lame.com>
2021-01-23 16:21:00 +0000

    Allow full markdown render inside figcaptions
    Turns out figcaptions can contain flow content, so getting the paragraph
    tags around a single line of content is actually allowed.  It also means
    the content of our figcaptions get the same typography styling as the rest
    of the document, which it otherwise wasn't.

--- config.rb
+++ config.rb
@@ -79,7 +79,7 @@ helpers do
   def audio_figure(src:, &block)
     concat_content %{<figure>
   <audio controls class="example-graphic" src="#{src}"></audio>
-  <figcaption markdown="span">#{capture_html(&block).strip}</figcaption>
+  <figcaption>#{markdown_render(capture_html(&block))}</figcaption>
 </figure>
 }
   end
@@ -87,7 +87,7 @@ helpers do
   def img_figure(src:, &block)
     concat_content %{<figure>
   <img class="example-graphic" src="#{src}">
-  <figcaption markdown="span">#{capture_html(&block).strip}</figcaption>
+  <figcaption>#{markdown_render(capture_html(&block))}</figcaption>
 </figure>
 }
   end
@@ -95,6 +95,11 @@ helpers do
   def markdown(inline: false, strip_tags: false, &block)
     raise ArgumentError, "Missing block" unless block_given?
     content = capture_html(&block)
+
+    concat markdown_render(content, inline: inline, strip_tags: strip_tags)
+  end
+
+  def markdown_render(content, inline: false, strip_tags: false)
     context = @app.template_context_class.new(@app, {}, {layout: false})
 
     rendered = ['erb', 'md'].reduce(content) do |rendered, template|
@@ -103,7 +108,7 @@ helpers do
     end
     rendered = rendered.gsub('<p>', '').gsub("</p>", '').gsub("\n", '') if inline
     rendered = strip_tags(rendered) if strip_tags
-    concat rendered
+    rendered
   end
 end
 
