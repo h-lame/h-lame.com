ee40a6353675e4ddc04b8714e46770e68afdb41e
Murray Steele <muz@h-lame.com>
2021-01-31 21:17:34 +0000

    Allow for tags in descriptions
    We can put a description in the meta by stripping the tags, but maybe we
    want the description elsewhere.

--- config.rb
+++ config.rb
@@ -52,8 +52,12 @@ helpers do
     yield_content(:title) || for_page.data.title
   end
 
+  def page_description(for_page = current_page)
+    markdown_render(for_page.data.fetch(:description, ''), inline: true)
+  end
+
   def page_meta_description(for_page = current_page)
-    for_page.data[:description]
+    markdown_render(for_page.data.fetch(:description, ''), inline: true, strip_tags: true)
   end
 
   def toc_name(for_page = current_page)
