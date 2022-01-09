571de6c1e321f632dfe532b4e546f8c4b7c57793
Murray Steele <muz@h-lame.com>
2022-01-09 16:51:09 +0000

    Allow multiple files to be passed to the image resizer
    Can't believe I was running this on each file manually like some kind of neanderthal.

--- scripts/notes_image_resizer.rb
+++ scripts/notes_image_resizer.rb
@@ -5,25 +5,32 @@ require 'pathname'
 
 desktop_size = 190
 mobile_size = 600
-raw_src = Pathname.new(ARGV[0])
-path = raw_src.dirname
-ext = raw_src.extname
-basename = raw_src.basename(ext)
+ARGV.each do |filename|
+  print "Resizing #{filename} ..."
+  raw_src = Pathname.new(filename)
+  path = raw_src.dirname
+  ext = raw_src.extname
+  basename = raw_src.basename(ext)
 
-[desktop_size, mobile_size].each do |image_size|
-  [1, 2].each do |scale|
-    new_file = path.join(basename.sub_ext(".#{image_size}-#{scale}x#{ext}"))
-    image_width = image_size * scale
-    ImageProcessing::Vips
-      .source(raw_src)
-      .resize_to_limit(image_width, nil)
-      .saver(quality: 95)
-      .custom do |vi|
-        vi.get_fields.each do |vif|
-          vi.remove vif unless vif == 'icc-profile-data'
+  [desktop_size, mobile_size].each do |image_size|
+    print "#{image_size}..."
+    [1, 2].each do |scale|
+      print "#{scale}..."
+      new_file = path.join(basename.sub_ext(".#{image_size}-#{scale}x#{ext}"))
+      image_width = image_size * scale
+      ImageProcessing::Vips
+        .source(raw_src)
+        .resize_to_limit(image_width, nil)
+        .saver(quality: 95)
+        .custom do |vi|
+          vi.get_fields.each do |vif|
+            vi.remove vif unless vif == 'icc-profile-data'
+          end
+          vi
         end
-        vi
-      end
-      .call(destination: new_file.to_path)
+        .call(destination: new_file.to_path)
+    end
+    print ";"
   end
+  puts " Done!"
 end
