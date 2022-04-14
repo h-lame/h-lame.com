c7ab9f8efa383ab01389e2de9e8512a8c74c098d
Murray Steele <muz@h-lame.com>
2022-04-14 22:50:27 +0100

    Reduce counting burden on me for doing footnotes
    There's no real need, I don't think, for me to self-manage the footnote numbering as I can just have the computer count up every `fnrf` and `fndf` in turn.  I've left the versions that take an explicit argument so I don't (yet) have to re-write all my existing notes and talks that use footnotes.

--- helpers/footnote_helpers.rb
+++ helpers/footnote_helpers.rb
@@ -3,15 +3,24 @@ module FootnoteHelpers
     content_for(:footnotes, &block)
   end
 
-  def footnote_reference(number)
+  def footnote_reference(number=(self.footnotes_ref_count+=1))
     concat_content %{<a id="fn-#{number}-return" href="#fn-#{number}"><sup>#{number}</sup></a>}
   end
   alias :fnrf :footnote_reference
 
-  def footnote_definition(number, &block)
+  def footnote_definition(number=(self.footnotes_def_count+=1), &block)
     footnote = capture_html(&block)
 
     concat_content %{<a id="fn-#{number}"><sup>#{number}.</sup></a> #{footnote} <a href="#fn-#{number}-return"><sup>⏎</sup></a>}
   end
   alias :fndf :footnote_definition
+
+  def footnotes_ref_count
+    @footnotes_ref_count ||= 0
+  end
+  attr_writer :footnotes_ref_count
+  def footnotes_def_count
+    @footnotes_def_count ||= 0
+  end
+  attr_writer :footnotes_def_count
 end
