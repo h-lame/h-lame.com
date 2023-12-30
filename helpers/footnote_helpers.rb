ac91047c018f06009ab9aae5e7b3af2a41199e01
Murray Steele <muz@h-lame.com>
2023-12-30 16:20:08 +0000

    Allow for multiple references to the same footnote
    It's imperfect as we can't tell _which_ reference to the footnote the user came
    from so we just have to render all the return links. It's a good starting place.

--- helpers/footnote_helpers.rb
+++ helpers/footnote_helpers.rb
@@ -3,15 +3,26 @@ module FootnoteHelpers
     content_for(:footnotes, &block)
   end
 
-  def footnote_reference(number=(self.footnotes_ref_count+=1))
-    concat_content %{<a id="fn-#{number}-return" href="#fn-#{number}"><sup>#{number}</sup></a>}
+  def footnote_reference(number=(self.footnotes_ref_count+=1), multi_ref=nil)
+    return_id = "fn-#{number}-return"
+    return_id << "-#{multi_ref}" unless multi_ref.nil?
+    concat_content %{<a id="#{return_id}" href="#fn-#{number}"><sup>#{number}</sup></a>}
   end
   alias :fnrf :footnote_reference
 
-  def footnote_definition(number=(self.footnotes_def_count+=1), &block)
+  def footnote_definition(number=(self.footnotes_def_count+=1), multi_refs=nil, &block)
     footnote = capture_html(&block)
 
-    concat_content %{<a id="fn-#{number}"><sup>#{number}.</sup></a> #{footnote} <a href="#fn-#{number}-return"><sup>⏎</sup></a>}
+    return_links =
+      if multi_refs.nil?
+        %{<a href="#fn-#{number}-return"><sup>⏎</sup></a>}
+      else
+        1.upto(multi_refs).map do |ref|
+          %{<a href="#fn-#{number}-return-#{ref}"><sup>⏎ #{ref}</sup></a>}
+        end.join(' ')
+      end
+
+    concat_content %{<a id="fn-#{number}"><sup>#{number}.</sup></a> #{footnote} #{return_links}}
   end
   alias :fndf :footnote_definition
 
